545d98e75ae51b31a6d13652d11257183b5a3f67
valdoth <valdotsiarohasina@gmail.com>
2022-07-23 04:38:19 +0300

    first commmit

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    //baseURL: "http://localhost:8001"
-    baseURL: "https://git.heroku.com/dating-app-mern119.git"
+    // baseURL: "http://localhost:8001"
+    baseURL: "https://dating-app-mern119.herokuapp.com/"
 })
 
 export default instance
